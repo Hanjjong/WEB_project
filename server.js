a19603774f799d7a082a42ab6a9fec852a15ce75
Hanjjong <86454797+Hanjjong@users.noreply.github.com>
2022-11-16 22:03:29 +0900

    create mainpage
    create mainpage
    load css,img

--- server.js
+++ server.js
@@ -1,32 +1,48 @@
 const express = require('express')
-const app = express
+const app = express();
 const mysql = require('mysql')
 const bodyParser = require('body-parser')
 var router = express.Router()
+app.use(express.static(__dirname + '/public'));
+
+
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '패스워드',
+    password: '1234',
     database: 'user',
     port: '3307',
   });
 
-  app.use(bodyParser.urlencoded({
-    extended: false,
+app.use(bodyParser.urlencoded({
+  extended: false,
   }));
   
-  app.listen(3307, () => {
-    console.log('Server is running port 3307!');
+app.listen(3307, () => {
+  console.log('Server is running port 3307!');
     // 데이터베이스 연결
-    connection.connect();
+  connection.connect();
   });
   
- 
-  app.set('view engine', 'ejs')
-  app.use(express.json())
-  app.use(express.urlencoded({extended:true}))
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
-  app.get('/', function(req, res){
-    res.render('mainpage.ejs');//res.sendFile(__dirname+'/~.html)
+app.get('/', function(req, res){
+  res.render('mainpage.ejs');//res.sendFile(__dirname+'/~.html)
+});
+
+app.get('/login', function(req, res){
+  res.render('login.ejs');
+});
+app.get('/', function(req, res){
+  res.render('mypage.ejs');
+});
+app.get('/member', function(req, res){
+  res.render('regist.ejs');
+});
+app.get('/member', function(req, res){
+  res.render('template.ejs');
 });
